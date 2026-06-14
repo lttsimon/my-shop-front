347a4295a244b8821ab05c275090f854d56d104d
ltt <3103840488@qq.com>
2026-06-14 13:16:04 +0800

    添加自动部署流水线

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-shop-front/',
   plugins: [vue()],
   compilerOptions: {
     // ...
